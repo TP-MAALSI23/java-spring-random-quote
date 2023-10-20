23c17e0b9fa11c65392c26c627256e09b2295fa4
Younes Yousfi <yuyu1403@gmail.com>
2023-10-20 15:05:52 +0200

    first

---
+++ src/main/java/com/testCICD/demo/Api.java
@@ -0,0 +1,50 @@
+package com.testcicd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class Api {
+    public static void main(String[] args) {
+        SpringApplication.run(Api.class, args);
+    }
+}
+
+@RestController
+class CitationController {
+
+    private static final String FILE_PATH = "citations.txt";
+    private final Random random = new Random();
+
+    @GetMapping("/citation")
+    public String getRandomCitation() {
+        try {
+            Resource resource = new ClassPathResource(FILE_PATH);
+            InputStream is = resource.getInputStream();
+            List<String> lines = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))
+                    .lines()
+                    .collect(Collectors.toList());
+
+            if (lines.isEmpty()) {
+                return "No citations found!";
+            }
+            return lines.get(random.nextInt(lines.size()));
+        } catch (IOException e) {
+            // Log the error if you have a logger. For now, just returning the error message.
+            return "Error reading citations: " + e.getMessage();
+        }
+    }
+}
